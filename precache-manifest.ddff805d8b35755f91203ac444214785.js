e998a971f4a6cd457d39f3861faeb2a5abaff373
AnuradhaSD <anuradha.sanjeewa@my.sliit.lk>
2020-04-23 15:34:53 +0530

    Updates

---
+++ precache-manifest.ddff805d8b35755f91203ac444214785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4b6a603fb7d7c84add06ccb19fdf7d",
+    "url": "/testApp/index.html"
+  },
+  {
+    "revision": "d15877c965b34976032f",
+    "url": "/testApp/static/css/main.b93a67c6.chunk.css"
+  },
+  {
+    "revision": "48de76597e87878bfc24",
+    "url": "/testApp/static/js/2.fe8c9100.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testApp/static/js/2.fe8c9100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d15877c965b34976032f",
+    "url": "/testApp/static/js/main.24e60056.chunk.js"
+  },
+  {
+    "revision": "7f97a5c8f49b67067de0",
+    "url": "/testApp/static/js/runtime-main.0a2c1a47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
